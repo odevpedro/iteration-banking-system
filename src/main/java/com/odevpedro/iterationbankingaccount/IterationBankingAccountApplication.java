1231f73dcbbadd0eee2c7b14dd3ae9da1577a16b
odevpedro <pedrosschmidt2@gmail.com>
2022-12-26 13:44:17 -0300

    initial files

---
+++ src/main/java/com/odevpedro/iterationbankingaccount/IterationBankingAccountApplication.java
@@ -0,0 +1,13 @@
+package com.odevpedro.iterationbankingaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterationBankingAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterationBankingAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odevpedro/iterationbankingaccount/IterationBankingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odevpedro.iterationbankingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterationBankingAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
